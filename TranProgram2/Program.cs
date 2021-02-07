972e005e1b2ee5763c715f26bd66fbbe66fa1783
ttran970406 <ttran65@cnm.edu>
2021-02-06 22:22:22 -0700

    Add project files.

---
+++ TranProgram2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TranProgram2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
